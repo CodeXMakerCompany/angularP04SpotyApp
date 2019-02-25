003c35ec3399d4f5af9a59f5d3ec6be25027edb3
codexmaker <codexmaker12@gmail.com>
2019-02-25 12:44:42 -0600

    Api de Spotify

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//Rutas
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+//Rutas
+import { ROUTES } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash:true } )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
